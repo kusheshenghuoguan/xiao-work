9554191bc3ab04e97023a1e0e8a0c4cbcaf42a3e
kusheshenghuoguan <56211120+kusheshenghuoguan@users.noreply.github.com>
2022-11-12 15:07:32 +0800

    Add files via upload

---
+++ 11122.java
@@ -0,0 +1,36 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>菜鸟教程(runoob.com)</title>
+</head>
+<body>
+<div style="
+ backgroung-color: #f1f1f1;
+ text-align: center;
+ padding: 40px;
+">
+	<img src="https://www.bilibili.com/favicon.ico"ait="袁晓" width="80px" hegith="80px"
+/div>
+	<div style="
+				max-width:800px;
+				margin:70px auto;
+				padding: 15px;
+				line-height: 1.7;
+				color: blue;
+				">
+	<p>姓名:yuanxiao</p>
+	<p>主题</p>
+	<p>经历</p>
+	<p>历程</p>
+	</div>
+	<div style="
+		backgroung-color:#f1f1f1;
+			text-align:center;
+			padding:40px;
+				font-size:12px；
+			">
+<p>我的网站</p>
+		<a href=https://space.bilibili.com/382843608?spm_id_from>友情链接</a>
+	</div>
+<body>
+<html>
